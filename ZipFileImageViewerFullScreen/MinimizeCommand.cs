756ff68a250f59f864bb3e586f50385f5f948015
Aosamesan <bluerein_@naver.com>
2016-03-07 18:27:36 +0900

    foo

---
+++ ZipFileImageViewerFullScreen/MinimizeCommand.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace ZipFileImageViewerFullScreen
+{
+    internal class MinimizeCommand : RelayCommand
+    {
+        public event Func<WindowState> GetCurrentState;
+        public event Action MinimizeAction;
+
+        public override bool CanExecute(object parameter)
+        {
+            return GetCurrentState?.Invoke() != WindowState.Minimized;
+        }
+
+        public override void Execute(object parameter)
+        {
+            MinimizeAction?.Invoke();
+        }
+    }
+}
---
+++ ZipFileImageViewerFullScreen/WindowCloseCommand.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ZipFileImageViewerFullScreen
+{
+    internal class WindowCloseCommand : RelayCommand
+    {
+        public event Action CloseAction;
+
+        public override bool CanExecute(object parameter)
+        {
+            return true;
+        }
+
+        public override void Execute(object parameter)
+        {
+            CloseAction?.Invoke();
+        }
+    }
+}
